e46160b466b095b8190d6eed0168398594d144e4
Sultan Javier <sultan.javier@gmail.com>
2024-03-08 14:07:47 -0800

    contak

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home.js';
 import About from './pages/about/about.js';
 import Products from './pages/product/product.js';
+import Contact from './pages/contact/contact.js';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/"  element={<Home/>} />
           <Route path="/aboutus"  element={<About/>} />
           <Route path="/product"  element={<Products/>} />
+          <Route path="/contact"  element={<Contact/>} />
       </Routes>
     </Router>
   );
